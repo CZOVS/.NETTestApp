189f2ac5d63a7d7aca6bf062b05174a16aa75c30
CZOVS <casper.zoeteman@ovsoftware.com>
2019-05-07 14:22:36 +0200

    Add project files.

---
+++ TestApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp
+{
+    class Program
+    {
+        //ConnectionString: Server=localhost;Database=master;Trusted_Connection=True;
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hallo GitHub!");
+            Console.ReadLine();
+
+        }
+    }
+}
